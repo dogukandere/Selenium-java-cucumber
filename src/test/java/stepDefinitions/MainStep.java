4ff1cf48cfd833d9d3e304528dab48cfee5f29da
Doğukan Dere <102256801+dogukandere@users.noreply.github.com>
2023-08-27 12:17:16 +0300

    change

--- src/test/java/stepDefinitions/MainStep.java
+++ src/test/java/stepDefinitions/MainStep.java
@@ -3,7 +3,8 @@ package stepDefinitions;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.testng.Assert;
+import static org.testng.Assert.*;
+
 import pages.MainPage;
 import utilities.DriverFactory;
 
@@ -20,27 +21,27 @@ public class MainStep {
 
     @When("check the bus button")
     public void check_the_bus_button()  {
-       Assert.assertTrue(mainPage.isBusButtonDisplayed());
+       assertTrue(mainPage.isBusButtonDisplayed());
     }
 
     @Then("check the plane button")
     public void check_the_plane_button() {
-        Assert.assertTrue(mainPage.isPlaneButtonDisplayed());
+        assertTrue(mainPage.isPlaneButtonDisplayed());
     }
 
     @Then("check the hotel button")
     public void check_the_hotel_button() {
-        Assert.assertTrue(mainPage.isHotelButtonDisplayed());
+        assertTrue(mainPage.isHotelButtonDisplayed());
     }
 
     @Then("check the car button")
     public void check_the_car_button() {
-        Assert.assertTrue(mainPage.isCarButtonDisplayed());
+        assertTrue(mainPage.isCarButtonDisplayed());
     }
 
     @Then("check the ferryboat button")
     public void check_the_ferryboat_button() {
-        Assert.assertTrue(mainPage.isFerryboatButtonDisplayed());
+        assertTrue(mainPage.isFerryboatButtonDisplayed());
     }
 
     @When("click plane button")
@@ -55,16 +56,16 @@ public class MainStep {
 
     @Then("check morning button")
     public void checkMorningButton() {
-        Assert.assertTrue(mainPage.isMorningButtonDisplayed());
+        assertTrue(mainPage.isMorningButtonDisplayed());
     }
 
     @Then("check noon button")
     public void checkNoonButton() {
-        Assert.assertTrue(mainPage.isNoonButtonDisplayed());
+        assertTrue(mainPage.isNoonButtonDisplayed());
     }
 
     @Then("check evening button")
     public void checkEveningButton() {
-        Assert.assertTrue(mainPage.isEveningButtonDisplayed());
+        assertTrue(mainPage.isEveningButtonDisplayed());
     }
 }
